d113497aedfa3e0a51ec7bd7889724e9f31405f3
fperret <fperret@student.42.fr>
2019-08-23 23:23:30 +0200

    start

---
+++ MiniJam35/Assets/ObstacleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+
+	public GameObject wood;
+
+	IEnumerator spawnWood()
+	{
+		while (true)
+		{
+			GameObject tmpWood = Instantiate(wood, new Vector3(7, Random.Range(0, 10), 0), transform.rotation);
+			yield return new WaitForSeconds(2);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(spawnWood());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MiniJam35/Assets/boat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boat : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate(Vector2.up * Time.deltaTime * speed);
+
+		}
+		else if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.Translate(Vector2.down * Time.deltaTime * speed);
+		}
+	}
+}
